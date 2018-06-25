3bf2bc03608a456bc94490b14ee16d655d4b429d
Del <olga.kornilova@me.com>
2018-06-25 17:53:31 +0300

    hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +9,43 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.setAll(storage, null );
     }
 
     void save(Resume r) {
+        int pos = storage.length;
+        storage[pos +1] = r;
+
     }
 
     Resume get(String uuid) {
-        return null;
+        List<Resume>  resumeList = Arrays.asList(getAll());
+        Resume r = ((Resume) resumeList.stream().filter(resume -> resume.uuid.equals(uuid)));
+        return r;
     }
 
     void delete(String uuid) {
+        List<Resume>  resumeList = Arrays.asList(getAll());
+        Resume r = ((Resume) resumeList.stream().filter(resume -> resume.uuid.equals(uuid)));
+        resumeList.remove(r);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        List<Resume> resultStorage = new ArrayList<>();
+        for(Resume r : storage)
+        {
+            if (r!=null)
+            {
+                resultStorage.add(r);
+            }
+        }
+        return (Resume[])resultStorage.toArray();
     }
 
     int size() {
-        return 0;
+        return getAll().length;
     }
 }
